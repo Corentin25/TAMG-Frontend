8a9655c7eeb4099f7113a308a45c1874fbca91aa
Corentin Busi <corentinbusi@gmail.com>
2026-06-08 10:55:20 +0200

    Intégration de la page Error 404

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 import "./error404.css";
 
 export function Error404() {
   return (
-    <>
-      <div>Error 404 page</div>
-    </>
+    <div className="error-container">
+      <h1>Erreur 404</h1>
+      <p>Oups ! Cette page n'existe pas...</p>
+      <Link to="/dashboard" className="back-button">
+        Revenir sur la page d'accueil
+      </Link>
+    </div>
   );
 }
